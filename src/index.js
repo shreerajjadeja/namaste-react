ff6ef4905f1c87e18dd983b7ef7c382f73e3c248
Shreeraj <shreeraj.rz@gmail.com>
2022-11-28 17:18:11 +0530

    SESSION 02
    Initialized NPM | Installed React libraries | Added browserlists | Script commands using Parcel | Added .gitignore file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const reactHeading = React.createElement('h1', {id: 'heading'}, 'Namaste React via React')
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+const reactHeading = React.createElement('h1', {id: 'heading'}, 'Namaste React loaded via React')
 
 const reactRoot = ReactDOM.createRoot(document.getElementById('react'))
 
